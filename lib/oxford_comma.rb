3a8620dea8f2269f59c1f7f20580a02d9e118be9
Brennan Basinger <bdbasinger777@gmail.com>
2018-07-21 23:52:08 -0500

    Finished

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    last = array[-1]
+    array.pop
+    str = array.join(', ') << ", and #{last}" 
+  end
 end
+
+a = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+
+puts oxford_comma(a) # kiwi, durian, starfruit, mangos, and dragon fruits
+
+
+
+
+
+
+
+
+
+
+
+
+
